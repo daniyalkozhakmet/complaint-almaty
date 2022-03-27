95fd2c3e0486c43fbf97910880551d7f7fd50941
daniyalkozhakmet <daniyalkozha1@gmail.com>
2022-03-27 20:44:14 +0600

    first commit

--- backend/settings.py
+++ backend/settings.py
@@ -93,10 +93,15 @@ REST_FRAMEWORK = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd49m0j4i1j4bs4',
+        'USER': 'mwqsbpesfeflvt',
+        'PASSWORD': '8d4b2a6881a0fee5b38d8a4c4da6749a9f1441b13200f067ce64cd426bce0201',
+        'HOST': 'ec2-52-3-60-53.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
+
 from datetime import timedelta
 
 SIMPLE_JWT = {
